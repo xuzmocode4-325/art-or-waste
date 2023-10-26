5c7ddc52769aa2413b75195353b91e85702c1309
Katlego Thobye <katlegothobye@yahoo.com>
2023-10-26 15:03:23 +0200

    updated model to resnet 34

--- app.py
+++ app.py
@@ -8,30 +8,28 @@ if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
 st.set_page_config(page_title="Art or Waste")
 
-classes = ['fine art','food waste','glass waste','metal waste','paper waste','plastic waste']
+classes = ['scrap paper', 'scrap metal', 'waste plastic', 'food scraps', 'scrap wood',
+               'visual art', 'pottery', 'wooden furniture', 'jewelery', 'plastic toy', 'glass waste']
 
 path = Path()
 
 if path.ls(file_exts='.pkl'):
-    learn_inf = load_learner(path/'model.pkl')
+    learn_inf = load_learner(path/'learner.pkl')
 
 with st.sidebar:
     st.title("""Do Not Trash Your Art Just Yet ♻♻♻""")
-    st.markdown("""This fun project classifies images as either `fine art` or one of five types of 
-                waste:""")
-    st.write("`food`")
-    st.write("`glass`")
-    st.write("`metal`")
-    st.write("`paper`")
-    st.write("`plastic`")
-    st.markdown("In future more art categories will be added.")
+    st.markdown("""This fun project classifies objects in images as either `scrap paper`, 
+                `scrap metal`, `waste plastic`, `food scraps`, `scrap wood`, `glass waste`, `visual art`, 
+                `pottery`, `wooden furniture`, `jewelery` or `plastic toy`.""")
+    st.markdown("In future more art or waste categories may be added.")
     st.markdown("""In production, such a model can be used as part of a mechanical waste recycling 
                 drive-chain that automatically sorts your trash for further processing, 
                 and alerts you when you could have possibly discarded something of value by mistake.""")
-    st.markdown("""This project is still in its infancy, so if your art gets missclassified as trash, 
-                please attach it to an email and send it through to artorwaste@gmail.com.""")
-    st.markdown("""The model will be retrained with your art so that it can better able to 
-                recognise your style of ingenuity in futute.""")
+    st.markdown("""This project is still in its infancy, we value your feedback and suggestions. 
+                If your art gets missclassified as trash, please attach it to an email and send it through to 
+                artorwaste@gmail.com.""")
+    st.markdown("""We continously retrain our model to get better predictions. Any art or trash images 
+                submissions will help to make our algorithm more accurate and unbiased.""")
 
 st.title("Art or Waste")
 
@@ -39,23 +37,22 @@ file_name = st.file_uploader("Upload an artwork in any image format.")
 
 if file_name is not None:
     col1, col2 = st.columns([2, 1], gap="small")
-
     try:
         Image.open(file_name)
     except RuntimeError:
-        st.warning("There was an error running the app. Please try again. If the error persists, try uploading another file.")
+        st.warning("There seems to be an error processing that file. Please try again. If the error persists, try uploading a different image.")
     else:
         image = Image.open(file_name)
     col1.image(image, use_column_width=True)
-    predictions = learn_inf.predict(image)
 
-    col1.header(f"""Result: `{predictions[0].title()}`""")
-    
-    col2.write("### Probabilites")
-    col2.markdown(f"""
-    {classes[0]}: {round(float(predictions[2][0]) * 100, 2)}% \n
-    {classes[1]}: {round(float(predictions[2][1]) * 100, 2)}% \n 
-    {classes[2]}: {round(float(predictions[2][2]) * 100, 2)}% \n
-    {classes[3]}: {round(float(predictions[2][3]) * 100, 2)}% \n
-    {classes[4]}: {round(float(predictions[2][4]) * 100, 2)}% \n
-    """)
+    try:
+        prediction = learn_inf.predict(image)
+    except RuntimeError:
+        st.warning("There seems to be an error processing that file. Please try again. If the error persists, try uploading a different image.")
+    else:
+        title = prediction[0].title()
+        vector = int(prediction[1])
+        col1.header(f"""Result: `{title}`""")
+        
+        col2.subheader("Probability")
+        col2.write(f"""### {round(float(prediction[2][vector]) * 100, 2)}% """)
